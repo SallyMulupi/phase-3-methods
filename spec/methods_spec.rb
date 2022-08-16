986135d9cd79158ed77ab8f1ef5f920ed21be253
SallyMulupi <mulupisally2gmail.com>
2022-08-16 21:31:47 +0300

    done

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,54 +1,26 @@
-require_relative 'spec_helper'
-require_relative '../methods'
+# /*
+#  
 
-describe '#greet_programmer' do
-
-  it 'outputs the string "Hello, programmer!"' do
-    expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
+def greet_programmer()
+  puts "Hello, programmer!"
   end
   
-end
-
-describe '#greet' do
-
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
+  def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+  end    
+  def greet(name)
+  puts "hello, #{name}!"
   end
-
-  it 'outputs a string "Hello, Jimmy!" when called with "Jimmy"' do
-    expect { greet("Jimmy") }.to output(a_string_including("Hello, Jimmy!")).to_stdout
-  end
-
-end
-
-describe '#greet_with_default' do
-
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, programmer!" when called with no arguments' do
-    expect { greet_with_default }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-
-end
-
-describe '#add' do
-
-  it 'returns the sum of two numbers' do
-    expect(add(2, 5)).to be(7)
-  end
-
-end
-
-describe '#halve' do
   
-  it 'returns half of the given integer' do
-    expect(halve(6)).to be(3)
+  
+  def add(num1, num2) 
+       return num1 + num2;
   end
-
-  it 'returns nil if not given a integer' do
-    expect(halve("six")).to be(nil)
-  end
-
-end
+  
+  
+    def halve number
+      if number.class != Integer
+        return nil
+    end
+    return number /2
+  end    
